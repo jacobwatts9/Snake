f68e74f3f2a5370329ddea89bc09d9375e0663d8
jacobwatts9 <jbearwatts@gmail.com>
2021-04-14 09:31:42 -0600

    Changed gamePanel and GameFrame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -5,10 +5,11 @@ public class GameFrame extends JFrame
 	GameFrame()
 	{
 		GamePanel panel = new GamePanel();
-		this.add(panel);
 		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,54 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
 
-public class GamePanel {
+public class GamePanel extends JPanel implements ActionListener{
+
+	GamePanel()
+	{
+		
+	}
+	public void startGame()
+	{
+		
+	}
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+	public void draw(Graphics g)
+	{
+		
+	}
+	public void move()
+	{
+		
+	}
+	public void checkApple()
+	{
+		
+	}
+	public void checkCollisions()
+	{
+		
+	}
+	public void gameOver(Graphics g)
+	{
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(keyEvent e)
+		{
+			
+		}
+	}
 
 }
